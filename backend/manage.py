738a546543c1abeeb6da196dac6ba2c3a4373ae2
Eric Mikulin <ericm99@gmail.com>
2020-02-29 14:36:42 -0800

    Cleanup after merge

--- backend/manage.py
+++ backend/manage.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
+from decouple import config
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gcomx.settings.test')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+
+if __name__ == "__main__":
+    settings_module = config('DJANGO_SETTINGS_MODULE', default=None)
+
+    # Check that folders exist
+    os.makedirs("mediafiles/images", exist_ok=True)
+    os.makedirs("mediafiles/objects", exist_ok=True)
+    os.makedirs("mediafiles/tmp_images", exist_ok=True)
+    os.makedirs("staticfiles", exist_ok=True)
+
+    # Check environment and settings
+    if sys.argv[1] == 'test':
+        if settings_module:
+            print("Ignoring config('DJANGO_SETTINGS_MODULE') because it's test. "
+                  "Using 'gcomx.settings.test'")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gcomx.settings.test")
+    else:  # pragma: no cover
+        if settings_module is None:
+            print("Error: no DJANGO_SETTINGS_MODULE found. Will NOT start devserver. "
+                  "Remember to create .env file at project root. "
+                  "Check README for more info.")
+            sys.exit(1)
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+    from django.core.management import execute_from_command_line
+
+
+
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
--- backend/virtualenv_name/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/claire/UAS/gcom-x/backend/virtualenv_name/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
