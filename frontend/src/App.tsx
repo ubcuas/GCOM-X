1db115e28b86a1455156a0f61437d0a735ea599e
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-03-05 14:08:46 -0800

    Hide scrollbars globally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import store from "./store/store";
 import { RootState } from "./store/reducers";
 
 import { CssBaseline, Grid, Theme } from "@mui/material";
+import { GlobalStyles } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 
 import Navbar from "./components/navbar/Navbar";
@@ -14,6 +15,7 @@ import Telemetry from "./components/telemetry/Telemetry";
 import Logs from "./components/logs/Logs";
 
 import { THEMES } from './utils/constants/THEMES';
+import { NoEncryption } from "@mui/icons-material";
 
 const ThemedApp = () => {
 
@@ -24,6 +26,12 @@ const ThemedApp = () => {
 
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles
+        styles={{
+          '*::-webkit-scrollbar': {
+            display: 'none',
+          }
+        }}/>
       <CssBaseline />
       <Navbar />
       <Fragment>
