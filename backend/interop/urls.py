8a0811230e3b90a4a9df592ba64a497777360a3f
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-06-06 16:55:33 -0700

    Add necessary slashes

--- backend/interop/urls.py
+++ backend/interop/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     url(r'api/interop/login$', views.login, name='interop.login'),
     url(r'api/interop/mission$', views.mission, name='interop.mission'),
     url(r'api/interop/missions/(?P<mission_id>[0-9])/$', views.missions, name='interop.missions'),
-    url(r'api/interop/status$', views.status, name='interop.status'),
+    url(r'api/interop/status/$', views.status, name='interop.status'),
     url(r'api/interop/teams/$', views.teams, name='interop.teams'),
-    url(r'api/interop/telemetry$', views.telemetry, name='interop.telemetry'),
-    url(r'api/interop/teamtelemetry$', views.teams, name='interop.teamtelemetry'),
-    url(r'api/interop/telemstatus$', views.telem_status, name='interop.telem_status'),
-    url(r'api/interop/teamtelemstatus$', views.team_telem_status, name='interop.team_telem_status')
+    url(r'api/interop/telemetry/$', views.telemetry, name='interop.telemetry'),
+    url(r'api/interop/teamtelemetry/$', views.teams, name='interop.teamtelemetry'),
+    url(r'api/interop/telemstatus/$', views.telem_status, name='interop.telem_status'),
+    url(r'api/interop/teamtelemstatus/$', views.team_telem_status, name='interop.team_telem_status')
 ]
