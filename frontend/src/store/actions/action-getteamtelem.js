4ca1bcccfa7893f1f29b17ca9431a36ef064f58e
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-06-12 10:50:31 -0700

    Cleanup

--- frontend/src/store/actions/action-getteamtelem.js
+++ frontend/src/store/actions/action-getteamtelem.js
@@ -1,19 +1,5 @@
 import axios from "axios";
 
-// const defaultValue = [{
-//     "team_id": 2,
-//     "latitude": 38.147192,
-//     "longitude": -76.428595,
-//     "uas_heading": 50,
-//     "altitude_msl": 23,
-// }, {
-//     "team_id": 3,
-//     "latitude": 38.145595,
-//     "longitude": -76.434636,
-//     "uas_heading": 130,
-//     "altitude_msl": 23,
-// }];
-
 const defaultValue = {
     "teams": 
         [
